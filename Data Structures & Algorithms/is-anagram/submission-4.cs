32c04766795bbcb7ab96310f5159600fd82225de
HassanQayyum1993 <33256057+HassanQayyum1993@users.noreply.github.com>
2026-05-01 22:41:59 +0500

    Format: Apply consistent C# formatting standards to is-anagram submission-4

--- Data Structures & Algorithms/is-anagram/submission-4.cs
+++ Data Structures & Algorithms/is-anagram/submission-4.cs
@@ -1,47 +1,52 @@
-public class Solution {
+public class Solution
+{
     public bool IsAnagram(string s, string t)
-{
-if(s.Length!=t.Length)
-{
-    return false;
-}
-    Dictionary<char, int> sdict = new Dictionary<char, int>();
-    Dictionary<char, int> tdict = new Dictionary<char, int>();
-
-    foreach (var item in s)
     {
-        if (sdict.ContainsKey(item))
-        {
-            sdict[item]++;
-        }
-        else
-        {
-            sdict[item] = 1;
-        }
-    }
-    foreach (var item in t)
-    {
-        if (tdict.ContainsKey(item))
-        {
-            tdict[item]++;
-        }
-        else
-        {
-            tdict[item] = 1;
-        }
-    }
-
-    foreach (var item in sdict)
-    {
-        if (!tdict.ContainsKey(item.Key))
+        if (s.Length != t.Length)
         {
             return false;
         }
-        if (sdict[item.Key] != tdict[item.Key])
+
+        Dictionary<char, int> sDict = new Dictionary<char, int>();
+        Dictionary<char, int> tDict = new Dictionary<char, int>();
+
+        foreach (var item in s)
         {
-            return false;
+            if (sDict.ContainsKey(item))
+            {
+                sDict[item]++;
+            }
+            else
+            {
+                sDict[item] = 1;
+            }
         }
+
+        foreach (var item in t)
+        {
+            if (tDict.ContainsKey(item))
+            {
+                tDict[item]++;
+            }
+            else
+            {
+                tDict[item] = 1;
+            }
+        }
+
+        foreach (var item in sDict)
+        {
+            if (!tDict.ContainsKey(item.Key))
+            {
+                return false;
+            }
+
+            if (sDict[item.Key] != tDict[item.Key])
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
-    return true;
-}
 }
