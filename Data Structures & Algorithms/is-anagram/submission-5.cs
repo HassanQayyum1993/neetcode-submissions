389dc5ee22bacf8c0049c319305a02e1b2ff9cd5
HassanQayyum1993 <33256057+HassanQayyum1993@users.noreply.github.com>
2026-05-03 13:07:13 +0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+
+        if (s.Length != t.Length) {
+            return false;
+        }
+        
+        Dictionary<char, int> sdict = new Dictionary<char, int>();
+        Dictionary<char, int> tdict = new Dictionary<char, int>();
+
+        foreach (var item in s) {
+            if (sdict.ContainsKey(item)) {
+                sdict[item]++;
+            } else {
+                sdict[item] = 1;
+            }
+        }
+
+        foreach (var item in t) {
+            if (tdict.ContainsKey(item)) {
+                tdict[item]++;
+            } else {
+                tdict[item] = 1;
+            }
+        }
+
+        foreach (var item in sdict) {
+            if (!tdict.ContainsKey(item.Key)) {
+                return false;
+            }
+            if (sdict[item.Key] != tdict[item.Key]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
